ee9eda9b8d9ec31aebb89fc4ad6a83385b88fdee
Ge Yang <kellyyangge@gmail.com>
2015-12-09 23:48:55 +1100

    add img

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$(function() {
+
+new WOW().init();
+$('#map').height($('.loc').height);
+	$('section :first-child').each(function(){
+		$(this).addClass("title");
+	});
+
+	$('section :nth-child(3n) ').each(function(){
+		$(this).addClass("title");
+	});
+
+	function initialize() {
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+          center: new google.maps.LatLng(-41.5591743,145.8636745),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+});
+
+
+
+
+
+
